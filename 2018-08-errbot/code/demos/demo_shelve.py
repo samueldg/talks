c7a71cf396dce0355893d4907e7587bed3c4b48c
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2023-01-24 23:14:36 -0500

    Remove duplicate code

--- 2018-08-errbot/code/demos/demo_shelve.py
+++ 2018-08-errbot/code/demos/demo_shelve.py
@@ -7,7 +7,6 @@ import datetime
 import os.path
 import shelve
 
-
 DB_FILENAME = "persistence.db"
 CURRENT_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 DB_FILEPATH = os.path.join(CURRENT_DIR_PATH, DB_FILENAME)
@@ -26,10 +25,3 @@ print(f"Shelve stored: {DB_FILEPATH}")
 with shelve.open(DB_FILEPATH) as store:
     for key, value in store.items():
         print(f"{key} => {value} ({type(value)})")
-    store["string"] = "Hello!"
-    store["now"] = datetime.datetime.utcnow()
-    store["int"] = 1337
-    store["null"] = None
-    store["dict"] = {
-        "a": 1.337,
-    }
